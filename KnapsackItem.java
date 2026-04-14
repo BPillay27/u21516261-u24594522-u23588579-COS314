7ea81f9ec89ec0aa3689d0d39f1b023c3b991f20
u23588579@tuks.co.za <u23588579@tuks.co.za>
2026-04-14 18:23:19 +0200

    Created the knapsack node basically

---
+++ KnapsackItem.java
@@ -0,0 +1,17 @@
+public class KnapsackItem {
+    private int weight;
+    private int value;
+
+    public KnapsackItem(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
