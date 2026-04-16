34e0e8989a2c3b02c3ab3dc3ecb9988ce0dc372c
Keegan-l-star <kllewis0911@gmail.com>
2026-04-16 16:06:25 +0200

    Should be finished

--- GA.java
+++ GA.java
@@ -5,6 +5,7 @@ public class GA{
     private boolean[] best;
     private boolean[][] pop;
     private long seed;
+    private Random rand;
 
     public GA(KnapsackInstance prob,long seed){
         this.prob=prob;
@@ -15,13 +16,79 @@ public class GA{
     }
 
     public boolean[] findSol(){
+        rand = new Random(this.seed);
+        Double pc=0.75;
         initialpop(); //The random starting population
 
+        //Fitness function is the prob.fit()
+        
+        boolean[][] survival; //This is the variable that contains the population that are selected
+        for(int i=0; i<10; i++){//Termination
+            survival = Selection();//Selection
+
+        //Crossover
+            if(rand.nextDouble()<pc){
+                survival=Crossover(survival);
+            }
+
+        //Mutation
+            for(int i=0;i<6;i++){
+                if(rand.nextDouble()<0.1){
+                    survival[i][rand.nextInt(prob.getTotalItems()-1)]=!survival[i][rand.nextInt(prob.getTotalItems()-1)];
+                }
+            }
+        //Population update
+            pop=survival;
+            update();
+        }
         return best;
     }
 
+    private void update(){
+        for(int i=0;i<6;i++){
+            if(best==null && pop[i]!=null){
+                best=pop[i];
+            }else
+            if(best!=null && pop!=null && pop[i]!=null && prob.isValid(pop[i])  && prob.fitness(best)<prob.fit(pop[i])){
+                best=pop[i];
+            }
+        }
+    }
+
+    private boolean[][] Selection(){//Roulette Wheel Selection
+        boolean[][] group=pop;
+        TreeMap<Double, boolean[]> wheel= new TreeMap<>();
+        Double percent=0;
+
+        for(int i=0;i<prop.getTotalItems(); i++){
+            wheel.put(percent, boolean[i]);
+            percent+=prop.fitness(i);
+        }
+
+        int position;
+        for(int i=0;i<prop.getTotalItems(); i++){
+            position= rand.nextInt(percent);
+            group[i]=wheel.floorEntry(position).getValue();
+        }
+
+        return group;
+    }
+
+     private boolean[][] Crossover(boolean[][] group){//Roulette Wheel Selection
+        boolean hold=false;
+        for(int i=0; i<(prob.getTotalItems()/2);i++){
+            hold =group[0][i];
+            group[1][i]=hold;
+            hold =group[2][i];
+            group[3][i]=hold;
+            hold =group[4][i];
+            group[5][i]=hold;
+        }
+        return group;
+    }
+
     private void initialpop(){
-        Random rand = new Random(this.seed);
+        
         for(int i=0;i<pop.length;i++){
             for(int j=0; j<pop[i].length; j++){
                 if (rand.nextDouble() < 0.15) {
@@ -30,12 +97,11 @@ public class GA{
                     pop[i][j]=false;
                 }
             }
-            if(!prob.isValid(pop[i])){
-                --i;// Stop the for loop from continuing until this is valid
+            if(!prob.isValid(pop[i]) ){
             }else{
                 if(best==null){
                     best=pop[i];
-                }else if(best==null && prob.fit(best)<prob.fit(pop[i])){
+                }else if(best!=null && prob.fitness(best)<prob.fit(pop[i])){
                     best=pop[i];
                 }
                  
