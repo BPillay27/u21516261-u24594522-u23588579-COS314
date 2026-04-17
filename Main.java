89f284f337a73ae0c724228ce23d5c5f558c37b4
Keegan-l-star <kllewis0911@gmail.com>
2026-04-17 13:58:54 +0200

    Fix the algorithm by increasing termination and started the tabel for Tshepiso

--- Main.java
+++ Main.java
@@ -267,7 +267,7 @@ public class Main {
     }
 
     private static void printsimpleResult(String file, GA al, KnapsackInstance inst) {
-        boolean[] solution = al.getSol(100);
+        boolean[] solution = al.getSol(165);
 
         if (solution == null) {
             System.out.println("GA returned no valid solution (null) for :" + file);
